22815774166b85dbde78b23cdd80cd716b0ab342
Deepa Pandey <deepapandey364@gmail.com>
2020-10-27 11:59:50 +0530

    diidd it

--- detectWifi.py
+++ detectWifi.py
@@ -1,4 +1,6 @@
->>> import subprocess
+>>> import subprocess   
 >>> devices = subprocess.check_output(['netsh','wlan','show','network'])
 >>> devices = d.decode('ascii')
 >>> print(devices)
+
+Note:  subprocess allows to spawn new processes, connect to their input/output/error pipes, and obtain their return codes. dont need pip to install it as the subprocess module comes preinstalled.

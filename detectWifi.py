29f5ecd66d3d0c82b73dc538dd1c45f8fd23b9c9
Deepa Pandey <deepapandey364@gmail.com>
2020-10-28 23:45:10 +0530

    Update detectWifi.py

--- detectWifi.py
+++ detectWifi.py
@@ -3,4 +3,5 @@
 >>> devices = d.decode('ascii')
 >>> print(devices)
 
-Note:  subprocess allows to spawn new processes, connect to their input/output/error pipes, and obtain their return codes. dont need pip to install it as the subprocess module comes preinstalled.
+Note:  subprocess allows to spawn new processes, connect to their input/output/error pipes, and obtain their return codes. 
+Don't need pip to install it as the subprocess module comes preinstalled.
